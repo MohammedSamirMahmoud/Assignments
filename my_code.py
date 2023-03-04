0687a46266bafb1e8b199a5c6c8def37bfb31170
MohammedSamirMahmoud <mohammed.samir.mahmoud@gmail.com>
2023-03-04 14:23:40 +0200

    Remmoved some lines

--- my_code.py
+++ my_code.py
@@ -5,11 +5,4 @@ def my_fun(x,z):
 
 def my_second_fn(y):
 	return class ClassName(object):
-		"""docstring for ClassName"""
-		def __init__(self, arg):
-			super(ClassName, self).__init__()
-			self.arg = arg
-			
-	return z
-
-	return 
+		
