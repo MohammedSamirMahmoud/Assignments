fff688e244a187c7ae95dfc6943484c4cb5634c7
MohammedSamirMahmoud <mohammed.samir.mahmoud@gmail.com>
2023-03-04 14:21:49 +0200

    Added my 2nd function

--- my_code.py
+++ my_code.py
@@ -1,3 +1,15 @@
 def my_fun(x,z):
 	z = x + z
 	return z 
+
+
+def my_second_fn(y):
+	return class ClassName(object):
+		"""docstring for ClassName"""
+		def __init__(self, arg):
+			super(ClassName, self).__init__()
+			self.arg = arg
+			
+	return z
+
+	return 
